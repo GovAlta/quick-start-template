031371b686ea98f661d9c5d6bc668387ee27b2a0
Andriy V. Koval <koval.andrey@gmail.com>
2023-08-24 09:38:34 -0600

    rename

--- analysis/report-example/source-code-layer.R
+++ analysis/report-example/source-code-layer.R
@@ -4,7 +4,7 @@ cat("\014") # Clear the console
 cat("Working directory: ", getwd()) # Must be set to Project Directory
 # Project Directory should be the root by default unless overwritten
 
-# ---- load-packages -----------------------------------------------------------
+#+ load-packages -----------------------------------------------------------
 # Choose to be greedy: load only what's needed
 # Three ways, from least (1) to most(3) greedy:
 # -- 1.Attach these packages so their functions don't need to be qualified: http://r-pkgs.had.co.nz/namespace.html#search-path
@@ -14,6 +14,9 @@ library(stringr)   # strings
 library(lubridate) # dates
 library(labelled)  # labels
 library(scales)    # format
+library(dplyr)     # loading dplyr explicitly is my guilty pleasure
+library(broom)     # for model
+library(emmeans)   # for interpreting model results
 # -- 2.Import only certain functions of a package into the search path.
 import::from("magrittr", "%>%")
 # -- 3. Verify these packages are available on the machine, but their functions need to be qualified: http://r-pkgs.had.co.nz/namespace.html#search-path
@@ -24,37 +27,36 @@ requireNamespace("janitor"  )# tidy data
 requireNamespace("dplyr"    )# Avoid attaching dplyr, b/c its function names conflict with a lot of packages (esp base, stats, and plyr).
 requireNamespace("testit"   )# For asserting conditions meet expected patterns.
 
-# ---- load-sources ------------------------------------------------------------
+#+ load-sources ------------------------------------------------------------
 base::source("./scripts/common-functions.R") # project-level
 
-# ---- declare-globals ---------------------------------------------------------
-# printed figures will go here:
-# prints_folder <- paste0("./analysis/.../prints/")
-# if(!file.exists(prints_folder)){dir.create(file.path(prints_folder))}
-
-# ---- declare-functions -------------------------------------------------------
+#+ declare-globals ---------------------------------------------------------
 # printed figures will go here:
 prints_folder <- paste0("./analysis/report-example/prints/")
-if (!fs::dir_exists(prints_folder)) {fs::dir_create(prints_folder)}
-# ---- load-data ---------------------------------------------------------------
+if(!file.exists(prints_folder)){dir.create(file.path(prints_folder))}
+
+#+ declare-functions -------------------------------------------------------
+
+
+#+ load-data ---------------------------------------------------------------
 ds0 <- readr::read_rds("../../data-public/raw/example-prosthetic-2.rds")
-# ---- inspect-data ------------------------------------------------------------
+#+ inspect-data ------------------------------------------------------------
 
 
-# ---- tweak-data --------------------------------------------------------------
+#+ tweak-data --------------------------------------------------------------
 
 
-# ---- table-1 -----------------------------------------------------------------
+#+ table-1 -----------------------------------------------------------------
 
 
-# ---- graph-1 -----------------------------------------------------------------
+#+ graph-1 -----------------------------------------------------------------
 
 
-# ---- graph-2 -----------------------------------------------------------------
+#+ graph-2 -----------------------------------------------------------------
 
-# ---- save-to-disk ------------------------------------------------------------
+#+ save-to-disk ------------------------------------------------------------
 
-# ---- publish ------------------------------------------------------------
+#+ publish ------------------------------------------------------------
 path <- "./analysis/.../report-isolated.Rmd"
 rmarkdown::render(
   input = path ,
