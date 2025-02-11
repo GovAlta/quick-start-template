c881ea1bd9cf9b6611931367043da1697f8726f9
andriy-koval_goa <koval.andrey@gmail.com>
2025-02-11 08:12:14 -0700

    Update operational-functions.R

--- scripts/operational-functions.R
+++ scripts/operational-functions.R
@@ -1,16 +1,14 @@
+library(tidyverse)
 # Functions loaded by SOME scripts in the project
-compute_fiscal_year <- function(x)(
-  # expects a date
-  as.integer(zoo::as.yearmon(x) - 3/12 )
-)
+compute_fiscal_year <- function(x)(as.integer(zoo::as.yearmon(x) - 3/12 ))
 
-compute_fiscal_quarter <- function(x){
-  # expects a date
-  q <- lubridate::quarter(x)
-  if_else(x == 1L ~ 4L,TRUE ~ x-1)
-}
-# Negation in vector selection
-`%not in%` <- Negate(`%in%`)
+# Compute difference in months
+# turn a date into a 'monthnumber' relative to an origin
+month_number <- function(d) { 
+  lt <- as.POSIXlt(as.Date(d, origin="1900-01-01")); lt$year*12 + lt$mon 
+} 
+# compute a month difference as a difference between two monnb's
+month_diff <- function(date1, date2) { month_number(date2) - month_number(date1)+1 }
 
 # Define bottom and right limits when import Excel sheet
 trim_sheet_input <- function(
@@ -42,6 +40,29 @@ draw_random_id <- function(d, idvar="person_oid", n = 1){
 }
 # ds_is %>% draw_random_id()
 # ds_ea %>% draw_random_id()
+
+keep_random_id <- function(d, idvar="person_oid", n = 1, seed=Sys.time()){
+  # browser()
+  set.seed(seed)
+  all_unique_ids <- d %>%
+    pull(!!rlang::sym(idvar)) %>%
+    unique()
+  a_random_id <- sample(all_unique_ids, size = n)
+  d_out <- d %>% filter(!!rlang::sym(idvar)%in%a_random_id)
+  return(d_out)
+}
+# How to use
+# ds %>% keep_random_id()
+
+
+show_random_case <- function(d, seed = Sys.time()){
+  set.seed
+  d %>% 
+    keep_random_id() %>% 
+    select(any_of(c("person_oid", "date_start", "date_end","program_class0", "program_class1", "intervention","interval_type","interval_num")))
+}
+# How to use
+# d %>% show_random_case
 
 # see https://www.tidyverse.org/blog/2019/06/rlang-0-4-0/
 # for details on using `.data[[]]` pronoun
@@ -92,24 +113,6 @@ drop_idvars <- function(d,add_name="none"){
   return(d_out)
 }
 
-# view as single case
-view_one <- function(
-    d
-    ,idvar    = "person_oid" # name of the variable to serve as unique identifier
-    ,case_id  = draw_random_id(d, n = 1)
-    ,add_case = NA # additional cases for review
-  ){
-  
-  known <- c(case_id)
-  added <- add_case
-  subset_cases <- c(known,added) %>% unique() %>% na.omit() %>% as.vector()
-  
-  d_out <- 
-    d %>% 
-    filter(
-      !!rlang::sym(idvar) %in% subset_cases
-    )
-}
 
 compare_freq <- function(d_full, d_sample, group_vars ){
   
